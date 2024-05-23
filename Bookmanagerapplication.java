ffb144861f4b533ae65635096dcc4f12215abb95
Vikas Kachhi <130860398+VIKASKACHHI@users.noreply.github.com>
2024-05-23 20:39:04 +0530

    Add files via upload

---
+++ Bookmanagerapplication.java
@@ -0,0 +1,12 @@
+package main.java.com.example.bookmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookManagerApplication.class, args);
+    }
+}
